289c94a9bda38b46ee916bf22685c128aaeaf7e0
dukpyoyang <dukpyoyang@gmail.com>
2022-08-16 10:38:43 +0900

    포스트 목록 페이지 작성자 정보 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from bs4 import BeautifulSoup
 from .models import Post
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+
+        self.user_trump = User.objects.create_user(
+            username='trump',
+            password='somepassword'
+        )
+        self.user_obama = User.objects.create_user(
+            username='obama',
+            password='somepassword'
+        )
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -44,10 +54,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title='첫 번째 포스트 입니다.',
             content='Hello, World. We are the World.',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트 입니다.',
             content='저는 쌀국수를 좋아합니다. ',
+            author=self.user_trump
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -60,6 +72,9 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         # 3.4 "아직 게시물이 없습니다" 라는 문구가 없어야 한다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
 
     def test_post_detail(self):
         # 1.1. 포스트가 하나 있다.
